2fb08be0b1210f129b24a5daebb0e537c4cb05cb
Anant Narayanan <anant@kix.in>
2013-11-22 19:23:48 +0530

    Add Grunt and bower integrations

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+/* global module */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    uglify : {
+      app : {
+        files : {
+          'backbone-firebase.min.js' : ['backbone-firebase.js']
+        }
+      }
+    },
+
+    jshint : {
+      options : {
+        'bitwise' : true,
+        'boss'    : true,
+        'browser' : true,
+        'curly'   : true,
+        'devel'   : true,
+        'eqnull'  : true,
+        'globals' : {
+          'Backbone'            : false,
+          'Firebase'            : false
+        },
+        'globalstrict' : true,
+        'indent'       : 2,
+        'latedef'      : true,
+        'maxlen'       : 115,
+        'noempty'      : true,
+        'nonstandard'  : true,
+        'undef'        : true,
+        'unused'       : true,
+        'trailing'     : true
+      },
+      all : ['backbone-firebase.js']
+    },
+
+    watch : {
+      scripts : {
+        files : 'backbone-firebase.js',
+        tasks : ['default', 'notify:watch'],
+        options : {
+          interrupt : true
+        }
+      }
+    },
+
+    notify: {
+      watch: {
+        options: {
+          title: 'Grunt Watch',
+          message: 'Build Finished'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+
+  grunt.registerTask('build', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['build']);
+};
